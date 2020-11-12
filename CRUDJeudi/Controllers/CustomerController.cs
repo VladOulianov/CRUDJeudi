e8314081d071e5aef7f1a0ab381438add0a8154e
VladOulianov <64905404+VladOulianov@users.noreply.github.com>
2020-11-12 15:08:34 +0100

    model

---
+++ CRUDJeudi/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using CRUDJeudi.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUDJeudi.Controllers
+{
+    public class CustomerController : Controller
+    {
+
+        ShopEntities db = new ShopEntities();
+
+        // GET: Customer
+        public ActionResult CustomerList()
+        {
+            var customers = db.customers.ToList();
+            return View(customers);
+        }
+    }
+}
